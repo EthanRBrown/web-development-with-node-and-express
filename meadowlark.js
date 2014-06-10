86ebada5f1e01cdef59519ae74931cd7af6cc128
Ethan Brown <ethan.brown@popart.com>
2014-06-10 16:21:31 -0700

    Added support to run app clustered.

--- meadowlark.js
+++ meadowlark.js
@@ -336,8 +336,18 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log( 'Express started in ' + app.get('env') +
+function startServer() {
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log( 'Express started in ' + app.get('env') +
         ' mode on http://localhost:' + app.get('port') +
         '; press Ctrl-C to terminate.' );
-});
+    });
+}
+
+if(require.main === module){
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function to create server
+    module.exports = startServer;
+}
