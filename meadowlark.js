00cd35cc9f1b381cc900f2c0d0595aa206c69244
Ethan Brown <ethan.brown@popart.com>
2014-06-09 19:07:37 -0700

    Added test for About page; currently failing.

--- meadowlark.js
+++ meadowlark.js
@@ -24,7 +24,10 @@ app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	res.render('about', { fortune: fortune.getFortune() } );
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
 
 // 404 catch-all handler (middleware)
