15b7ebfea8373564430a8f3d3eb8cec0a0ffaf2e
Ethan Brown <e@zepln.com>
2014-06-09 18:21:33 -0700

    Minimal Express application.

---
+++ meadowlark.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');	
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
