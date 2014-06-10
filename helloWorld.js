3f677752c28eb147adebcc1bbe08ad522237aab3
Ethan Brown <e@zepln.com>
2014-06-09 17:55:06 -0700

    Added simple route parsing.

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,23 @@
 var http = require('http');
 
 http.createServer(function(req,res){ 
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello world!');
+	// normalize url by removing querystring, optional 
+	// trailing slash, and making it lowercase
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	switch(path) {
+		case '': 
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+			res.end('Homepage'); 
+			break;
+		case '/about': 
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+			res.end('About');
+			break;
+		default:
+			res.writeHead(404, { 'Content-Type': 'text/plain' });
+			res.end('Not found');
+			break;
+	}
 }).listen(3000);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
