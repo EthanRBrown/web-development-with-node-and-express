de6c6e95e41c8e72982f7dda8911ab2e34246e5e
Ethan Brown <e@zepln.com>
2014-06-09 17:59:31 -0700

    Serve static files (images, HTML).

--- helloWorld.js
+++ helloWorld.js
@@ -1,21 +1,36 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+	if(!responseCode) responseCode = 200;
+	fs.readFile(__dirname + path, function(err,data) {
+		if(err) {
+			res.writeHead(500, { 'Content-Type': 'text/plain' });
+			res.end('500 - Internal Error');
+		} else {
+			res.writeHead(responseCode, { 'Content-Type': contentType });
+			res.end(data);
+		}
+	});
+}
 
 http.createServer(function(req,res){ 
-	// normalize url by removing querystring, optional 
-	// trailing slash, and making it lowercase
-	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	// normalize url by removing querystring, optional
+	// trailing slash, and making lowercase
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '')
+		.toLowerCase();
 	switch(path) {
 		case '': 
-			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end('Homepage'); 
+			serveStaticFile(res, '/public/home.html', 'text/html');
 			break;
 		case '/about': 
-			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end('About');
+			serveStaticFile(res, '/public/about.html', 'text/html');
 			break;
-		default:
-			res.writeHead(404, { 'Content-Type': 'text/plain' });
-			res.end('Not found');
+		case '/img/logo.jpg': 
+			serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+			break;
+		default: 
+			serveStaticFile(res, '/public/404.html', 'text/html', 404);
 			break;
 	}
 }).listen(3000);
