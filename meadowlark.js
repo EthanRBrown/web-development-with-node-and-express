fb28b8c05ff6aa3be08c1c77f1ae44f0803a2342
Ethan Brown <ethan.brown@popart.com>
2014-06-10 16:36:52 -0700

    Added domain support for improved error handling.

--- meadowlark.js
+++ meadowlark.js
@@ -25,6 +25,51 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// use domains for better error handling
+app.use(function(req, res, next){
+    // create a domain for this request
+    var domain = require('domain').create();
+    // handle errors on this domain
+    domain.on('error', function(err){
+        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+        try {
+            // failsafe shutdown in 5 seconds
+            setTimeout(function(){
+                console.error('Failsafe shutdown.');
+                process.exit(1);
+            }, 5000);
+
+            // disconnect from the cluster
+            var worker = require('cluster').worker;
+            if(worker) worker.disconnect();
+
+            // stop taking new requests
+            server.close();
+
+            try {
+                // attempt to use Express error route
+                next(err);
+            } catch(err){
+                // if Express error route failed, try
+                // plain Node response
+                console.error('Express error mechanism failed.\n', err.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error.');
+            }
+        } catch(err){
+            console.error('Unable to send 500 response.\n', err.stack);
+        }
+    });
+
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
+
 // logging
 switch(app.get('env')){
     case 'development':
@@ -323,6 +368,12 @@ app.post('/cart/checkout', function(req, res){
     res.render('cart-thank-you', { cart: cart });
 });
 
+app.get('/epic-fail', function(req, res){
+    process.nextTick(function(){
+        throw new Error('Kaboom!');
+    });
+});
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
 	res.status(404);
@@ -336,8 +387,10 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
+var server;
+
 function startServer() {
-    http.createServer(app).listen(app.get('port'), function(){
+    server = http.createServer(app).listen(app.get('port'), function(){
       console.log( 'Express started in ' + app.get('env') +
         ' mode on http://localhost:' + app.get('port') +
         '; press Ctrl-C to terminate.' );
