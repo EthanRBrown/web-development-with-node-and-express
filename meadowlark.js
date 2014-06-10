9a04f1c60fa02fccb4dbd158444eb591b0ff8476
Ethan Brown <e@zepln.com>
2014-06-09 19:42:31 -0700

    Added 'current weather' partial with mocked-up weather data.

--- meadowlark.js
+++ meadowlark.js
@@ -20,6 +20,42 @@ app.use(function(req, res, next){
 	next();
 });
 
+// mocked weather data
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+ 	res.locals.partials.weather = getWeatherData();
+ 	next();
+});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
