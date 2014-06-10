01e3de822d97f587196edba5386ef071eb448f29
Ethan Brown <ethan.brown@popart.com>
2014-06-09 22:54:23 -0700

    Added vacation photo contest file upload form (not hooked up).

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	formidable = require('formidable');
 
 var app = express();
 
@@ -113,6 +114,21 @@ app.post('/process', function(req, res){
         // if there were an error, we would redirect to an error page
         res.redirect(303, '/thank-you');
     }
+});
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 // 404 catch-all handler (middleware)
