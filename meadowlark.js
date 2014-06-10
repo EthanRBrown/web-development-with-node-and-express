07e212064aa590dfd1810b58a5ca0727de8a5fdf
Ethan Brown <ethan.brown@popart.com>
2014-06-09 18:49:13 -0700

    Added Handlebars layout and views.

--- meadowlark.js
+++ meadowlark.js
@@ -10,28 +10,24 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-	res.type('text/plain');	
-	res.send('Meadowlark Travel');
+app.get('/', function(req, res) {
+	res.render('home');
 });
-app.get('/about', function(req, res){
-	res.type('text/plain');	
-	res.send('About Meadowlark Travel');
+app.get('/about', function(req, res) {
+	res.render('about');
 });
 
-// custom 404 page
-app.use(function(req, res){
-	res.type('text/plain');	
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
