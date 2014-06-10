3ad88553a332980aa214ea0889126b6d2578116b
Ethan Brown <ethan.brown@popart.com>
2014-06-09 18:38:30 -0700

    Added Handlebars view engine.

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
