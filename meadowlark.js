b8f3dcba2d149d99327775be55c610be85a91d17
Ethan Brown <ethan.brown@popart.com>
2014-06-09 19:18:57 -0700

    Added Oregon Coast Tour page.  All cross-page tests now passing.

--- meadowlark.js
+++ meadowlark.js
@@ -32,6 +32,9 @@ app.get('/about', function(req,res){
 app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
 });
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
 app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
