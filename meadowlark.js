392a0ce90447989a3cae48adbdaeef47410e50eb
Ethan Brown <e@zepln.com>
2014-06-11 19:56:23 -0700

    Added support for auto views.

--- meadowlark.js
+++ meadowlark.js
@@ -231,6 +231,23 @@ admin.get('/users', function(req, res){
 // add routes
 require('./routes.js')(app);
 
+// add support for auto views
+var autoViews = {};
+
+app.use(function(req,res,next){
+    var path = req.path.toLowerCase();  
+    // check cache; if it's there, render the view
+    if(autoViews[path]) return res.render(autoViews[path]);
+    // if it's not in the cache, see if there's
+    // a .handlebars file that matches
+    if(fs.existsSync(__dirname + '/views' + path + '.handlebars')){
+        autoViews[path] = path.replace(/^\//, '');
+        return res.render(autoViews[path]);
+    }
+    // no view found; pass on to 404 handler
+    next();
+});
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
 	res.status(404);
