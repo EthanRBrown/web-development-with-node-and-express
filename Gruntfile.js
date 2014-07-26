19bee6946f005a15443aa41485d6e5af7ac7382b
Ethan Brown <e@zepln.com>
2014-07-26 11:37:14 -0700

    Added static resource helper to LESS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt){
 		'grunt-cafe-mocha',
 		'grunt-contrib-jshint',
 		'grunt-exec',
+		'grunt-contrib-less',
 	].forEach(function(task){
 		grunt.loadNpmTasks(task);
 	});
@@ -21,6 +22,22 @@ module.exports = function(grunt){
 		exec: {
 			linkchecker: { cmd: 'linkchecker http://localhost:3000' }
 		},
+		less: {
+			development: {
+				options: {
+					customFunctions: {
+						static: function(lessObject, name) {
+							return 'url("' +
+								require('./lib/static.js').map(name.value) +
+								'")';
+						}
+					}
+				},
+				files: {
+					'public/css/main.css': 'less/main.less',
+				}
+			}
+		}
 	});	
 
 	// register tasks
