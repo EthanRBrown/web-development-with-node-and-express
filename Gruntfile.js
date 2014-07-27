51c4886664ec7cf4b8261dbe83ca5e4d1c4fad71
Ethan Brown <e@zepln.com>
2014-07-27 09:27:07 -0700

    Added bundling and minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,9 @@ module.exports = function(grunt){
 		'grunt-contrib-jshint',
 		'grunt-exec',
 		'grunt-contrib-less',
+		'grunt-contrib-uglify',
+		'grunt-contrib-cssmin',
+		'grunt-hashres',
 	].forEach(function(task){
 		grunt.loadNpmTasks(task);
 	});
@@ -35,11 +38,46 @@ module.exports = function(grunt){
 				},
 				files: {
 					'public/css/main.css': 'less/main.less',
+					'public/css/cart.css': 'less/cart.less',
 				}
 			}
-		}
+		},
+		uglify: {
+			all: {
+				files: {
+					'public/js.min/meadowlark.min.js': ['public/js/**/*.js']
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'public/css/meadowlark.css': ['public/css/**/*.css',
+					'!public/css/meadowlark*.css']
+				}
+			},
+			minify: {
+				src: 'public/css/meadowlark.css',
+				dest: 'public/css/meadowlark.min.css',
+			},
+		},
+		hashres: {
+			options: {
+				fileNameFormat: '${name}.${hash}.${ext}'
+			},
+			all: {
+				src: [
+					'public/js/meadowlark.min.js',
+					'public/css/meadowlark.min.css',
+				],
+				dest: [
+					'views/layouts/main.handlebars',
+				]
+			},
+		},
 	});	
 
 	// register tasks
 	grunt.registerTask('default', ['cafemocha','jshint','exec']);
+	grunt.registerTask('static', ['less', 'cssmin', 'uglify', 'hashres']);
 };
---
+++ public/js/contact.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	console.log('contact forms initialized');
+});
