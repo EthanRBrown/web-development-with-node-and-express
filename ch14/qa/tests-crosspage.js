d115a5119262443766f84b93c65cc609f6edcc33
= <e@zepln.com>
2015-06-20 19:17:52 -0400

    ch14: removed defunct cross-page tests.

--- ch14/qa/tests-crosspage.js
+++ ch14/qa/tests-crosspage.js
@@ -9,34 +9,7 @@ suite('Cross-Page Tests', function(){
 		browser = new Browser();
 	});
 
-	test('requesting a group rate quote from the hood river tour page should ' +
-			'populate the hidden referrer field correctly', function(done){
-		var referrer = 'http://localhost:3000/tours/hood-river';
-		browser.visit(referrer, function(){
-			browser.clickLink('.requestGroupRate', function(){
-				assert(browser.field('referrer').value === referrer);
-				done();
-			});
-		});
-	});
-
-	test('requesting a group rate from the oregon coast tour page should ' +
-			'populate the hidden referrer field correctly', function(done){
-		var referrer = 'http://localhost:3000/tours/oregon-coast';
-		browser.visit(referrer, function(){
-			browser.clickLink('.requestGroupRate', function(){
-				assert(browser.field('referrer').value === referrer);
-				done();
-			});
-		});
-	});
-
-	test('visiting the "request group rate" page dirctly should result ' +
-			'in an empty value for the referrer field', function(done){
-		browser.visit('http://localhost:3000/tours/request-group-rate', function(){
-			assert(browser.field('referrer').value === '');
-			done();
-		});
-	});
+	// we removed our old crosspage tests now that we've gotten rid of the "tours" pages;
+	// we should probably add new ones for the new "vacations" pages at some point!
 
 });
