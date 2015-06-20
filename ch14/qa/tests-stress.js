c7f07a5497455e981685f82a1550ed62caafb0c9
= <e@zepln.com>
2015-06-20 19:14:03 -0400

    Reduced stress testing requirements to serving 50 requests in under a second.

--- ch14/qa/tests-stress.js
+++ ch14/qa/tests-stress.js
@@ -3,11 +3,11 @@ var expect = require('chai').expect;
 
 suite('Stress tests', function(){
 
-      test('Homepage should handle 100 requests in a second', function(done){
+      test('Homepage should handle 50 requests in under a second', function(done){
           var options = {
               url: 'http://localhost:3000',
               concurrency: 4,
-              maxRequests: 100
+              maxRequests: 50,
           };
           loadtest.loadTest(options, function(err,result){
             expect(!err);
