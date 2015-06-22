8beda57e0a569a3955b4bacae57f635f412ffda1
= <e@zepln.com>
2015-06-21 20:17:04 -0400

    ch18: Added serialize/deserialize user methods for Passport.

---
+++ ch18/lib/auth.js
@@ -0,0 +1,14 @@
+var User = require('../models/user.js'),
+	passport = require('passport'),
+	FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.serializeUser(function(user, done){
+	done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.findById(id, function(err, user){
+		if(err || !user) return done(err, null);
+		done(null, user);
+	});
+});
