6ac48176d1a63a6dcffeb3c8ed78f804e51a26d3
= <e@zepln.com>
2015-06-28 23:10:21 -0400

    ch19: finished getAccessToken for Twitter integration.

--- ch19/lib/twitter.js
+++ ch19/lib/twitter.js
@@ -8,7 +8,37 @@ module.exports = function(twitterOptions){
 	// this function will be invisible outside of this module
 	function getAccessToken(cb){
 		if(accessToken) return cb(accessToken);
-		// TODO: get access token
+
+		var bearerToken = Buffer(
+			encodeURIComponent(twitterOptions.consumerKey) + ':' +
+			encodeURIComponent(twitterOptions.consumerSecret)
+		).toString('base64');
+
+		var options = {
+			hostname: 'api.twitter.com',
+			port: 443,
+			method: 'POST',
+			path: '/oauth2/token?grant_type=client_credentials',
+			headers: {
+				'Authorization': 'Basic ' + bearerToken,
+			},
+		};
+
+		https.request(options, function(res){
+			var data = '';
+			res.on('data', function(chunk){
+				data += chunk;
+			});
+			res.on('end', function(){
+				var auth = JSON.parse(data);
+				if(auth.token_type!=='bearer') {
+					console.log('Twitter auth failed.');
+					return;
+				}
+				accessToken = auth.access_token;
+				cb(accessToken);
+			});
+		}).end();
 	}
 
 	return {
