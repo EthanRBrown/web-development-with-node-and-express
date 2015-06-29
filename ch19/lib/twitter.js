86e98a667b084da5cb4360fd3e26522e10a29fc8
= <e@zepln.com>
2015-06-28 23:08:54 -0400

    ch19: stubbed out getAccessToken for Twitter integration.

--- ch19/lib/twitter.js
+++ ch19/lib/twitter.js
@@ -2,6 +2,15 @@ var https = require('https');
 
 module.exports = function(twitterOptions){
 
+	// this variable will be invisible outside of this module
+	var accessToken;
+
+	// this function will be invisible outside of this module
+	function getAccessToken(cb){
+		if(accessToken) return cb(accessToken);
+		// TODO: get access token
+	}
+
 	return {
 		search: function(query, count, cb){
 			// TODO
