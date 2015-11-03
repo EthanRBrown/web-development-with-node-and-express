874df8ad16b549db68a9098c79e093fc87058245
jpersson <jonathan.persson@creuna.se>
2015-11-02 19:22:05 -0500

    Refactor promises to use `Q.Promise` instead of `Q.defer`
    Since promises are now a part of ES2015 it’s better to use the
    [revealing constructor
    pattern](https://blog.domenic.me/the-revealing-constructor-pattern/)
    that more resembles the spec than the “deferred pattern” when creating
    promises.
    
    Reference:
    http://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html

--- ch19/meadowlark.js
+++ ch19/meadowlark.js
@@ -372,27 +372,26 @@ var getWeatherData = (function(){
     return function() {
         if( !c.refreshing && Date.now() > c.refreshed + c.updateFrequency ){
             c.refreshing = true;
-            var promises = [];
-            c.locations.forEach(function(loc){
-                var deferred = Q.defer();
-                var url = 'http://api.wunderground.com/api/' +
-                    credentials.WeatherUnderground.ApiKey +
-                    '/conditions/q/OR/' + loc.name + '.json';
-                http.get(url, function(res){
-                    var body = '';
-                    res.on('data', function(chunk){
-                        body += chunk;
-                    });
-                    res.on('end', function(){
-                        body = JSON.parse(body);
-                        loc.forecastUrl = body.current_observation.forecast_url;
-                        loc.iconUrl = body.current_observation.icon_url;
-                        loc.weather = body.current_observation.weather;
-                        loc.temp = body.current_observation.temperature_string;
-                        deferred.resolve();
+            var promises = c.locations.map(function(loc){
+                return Q.Promise(function(resolve){
+                    var url = 'http://api.wunderground.com/api/' +
+                        credentials.WeatherUnderground.ApiKey +
+                        '/conditions/q/OR/' + loc.name + '.json';
+                    http.get(url, function(res){
+                        var body = '';
+                        res.on('data', function(chunk){
+                            body += chunk;
+                        });
+                        res.on('end', function(){
+                            body = JSON.parse(body);
+                            loc.forecastUrl = body.current_observation.forecast_url;
+                            loc.iconUrl = body.current_observation.icon_url;
+                            loc.weather = body.current_observation.weather;
+                            loc.temp = body.current_observation.temperature_string;
+                            resolve();
+                        });
                     });
                 });
-                promises.push(deferred);
             });
             Q.all(promises).then(function(){
                 c.refreshing = false;
@@ -425,15 +424,14 @@ function getTopTweets(cb){
 
 	twitter.search('#travel', topTweets.count, function(result){
 		var formattedTweets = [];
-		var promises = [];
 		var embedOpts = { omit_script: 1 };
-		result.statuses.forEach(function(status){
-			var deferred = Q.defer();
-			twitter.embed(status.id_str, embedOpts, function(embed){
-				formattedTweets.push(embed.html);
-				deferred.resolve();
-			});
-			promises.push(deferred.promise);
+		var promises = result.statuses.map(function(status){
+            return Q.Promise(function(resolve){
+    			twitter.embed(status.id_str, embedOpts, function(embed){
+    				formattedTweets.push(embed.html);
+    				resolve();
+    			});
+            });
 		});
 		Q.all(promises).then(function(){
 			topTweets.lastRefreshed = Date.now();
