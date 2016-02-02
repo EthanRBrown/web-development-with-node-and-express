d0c40935c5e32de2bda5ddead703e8d0cc093b5b
Micah Powell <micah1powell@gmail.com>
2016-02-02 16:20:18 -0500

    Fixes typo on line 6

--- ch03/meadowlark.js
+++ ch03/meadowlark.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars')
+var handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
